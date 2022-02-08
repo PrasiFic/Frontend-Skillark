c55513df0cd6ec67a1e025f0f2413e6474d2348a
Prashant Pandey <67453546+PrasiFic@users.noreply.github.com>
2022-02-08 20:18:13 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,147 @@
+// Course Array
+
+const courses = [
+    // One course
+    { 
+        // Meta has basic details to be shown on the card, execpting price
+    "meta":{
+        "name":'React Js',
+       "logo":'www.ggole.come',
+        "sponser":"Skillak",
+        "type":"Live Training",
+        "price":"299",
+                "duration":"8 weeks"
+    },
+    // COurse's instructor profile
+    "":{
+        "name":"Mr. India",
+        "designation":"Jadoogar",
+        "organisation":"President of India",
+        "instructorMessgae":"Mr. India lives in india",
+        "linkedInProfile":"www.linkedin.com",
+        
+    },
+    // Skills focused in this course
+    skills:['Java', 'C++', 'Python'],
+    // Syllabus and projects covered in this course.
+    whatYouWillLean:{
+        
+        sections:[{sectionHeading:"Welcome to JAva", topicsToBeCovered:['What is Java', 'WHy JAva', 'How JAva']}],
+        projects:[{projectHeading:'Calculator using Core JAva', projectDetails:'In this project you will lesrn how to make calculator using core java', 'skillasEmphasized':['operators in Java', 'Input/Ouput oeprations']}]
+    }
+    // SSecond Course
+    
+
+    
+
+},
+// 2
+{ 
+    // Meta has basic details to be shown on the card, execpting price
+"meta":{
+
+    "name":'React Js',
+    "logo":'www.ggole.come',
+    "sponser":"Skillak",
+    "type":"Live Training",
+    "price":"299",
+            "duration":"8 weeks"
+},
+// COurse's instructor profile
+"instructor":{
+    "name":"Mr. India",
+    "designation":"Jadoogar",
+    "organisation":"President of India",
+    "instructorMessgae":"Mr. India lives in india",
+    "linkedInProfile":"www.linkedin.com",
+    
+},
+// Skills focused in this course
+skills:['Java', 'C++', 'Python'],
+// Syllabus and projects covered in this course.
+whatYouWillLean:{
+    
+    sections:[{sectionHeading:"Welcome to JAva", topicsToBeCovered:['What is Java', 'WHy JAva', 'How JAva']}],
+    projects:[{projectHeading:'Calculator using Core JAva', projectDetails:'In this project you will lesrn how to make calculator using core java', 'skillasEmphasized':['operators in Java', 'Input/Ouput oeprations']}]
+}
+// SSecond Course
+
+
+
+
+},
+// 3
+{ 
+    // Meta has basic details to be shown on the card, execpting price
+"meta":{
+
+    "name":'React Js',
+    "logo":'www.ggole.come',
+    "sponser":"Skillak",
+    "type":"Live Training",
+    "price":"299",
+            "duration":"8 weeks"
+},
+// COurse's instructor profile
+"instructor":{
+    "name":"Mr. India",
+    "designation":"Jadoogar",
+    "organisation":"President of India",
+    "instructorMessgae":"Mr. India lives in india",
+    "linkedInProfile":"www.linkedin.com",
+    
+},
+// Skills focused in this course
+skills:['Java', 'C++', 'Python'],
+// Syllabus and projects covered in this course.
+whatYouWillLean:{
+    
+    sections:[{sectionHeading:"Welcome to JAva", topicsToBeCovered:['What is Java', 'WHy JAva', 'How JAva']}],
+    projects:[{projectHeading:'Calculator using Core JAva', projectDetails:'In this project you will lesrn how to make calculator using core java', 'skillasEmphasized':['operators in Java', 'Input/Ouput oeprations']}]
+}
+// SSecond Course
+
+
+
+
+},
+// 4
+{ 
+    // Meta has basic details to be shown on the card, execpting price
+"meta":{
+
+    "name":'React Js',
+    "logo":'www.ggole.come',
+    "sponser":"Skillak",
+    "type":"Live Training",
+    "price":"299",
+            "duration":"8 weeks"
+},
+// COurse's instructor profile
+"instructor":{
+    "name":"Mr. India",
+    "designation":"Jadoogar",
+    "organisation":"President of India",
+    "instructorMessgae":"Mr. India lives in india",
+    "linkedInProfile":"www.linkedin.com",
+    
+},
+// Skills focused in this course
+skills:['Java', 'C++', 'Python'],
+// Syllabus and projects covered in this course.
+whatYouWillLean:{
+    
+    sections:[{sectionHeading:"Welcome to JAva", topicsToBeCovered:['What is Java', 'WHy JAva', 'How JAva']}],
+    projects:[{projectHeading:'Calculator using Core JAva', projectDetails:'In this project you will lesrn how to make calculator using core java', 'skillasEmphasized':['operators in Java', 'Input/Ouput oeprations']}]
+}
+// SSecond Course
+
+
+
+
+}
+
+]
+
+
+export {courses}
